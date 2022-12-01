601b879c89897295158503e2a438eed7bca4b190
adam.tal@vimeo.com <adam.tal@vimeo.com>
2022-12-01 10:24:08 -0500

    Day 1

--- day-1-calorie-counting/part-2.py
+++ day-1-calorie-counting/part-2.py
@@ -1,17 +1,25 @@
-
+import heapq
 
 elf_calorie_inventory_items = open('./input.txt').read().split('\n')
 
 highest_calories_held_by_a_single_elf = float('-inf')
 
 current_elf_calories = 0
+elf_calorie_totals_max_heap = []
 for elf_calorie_inventory_item in elf_calorie_inventory_items:
 
     if not elf_calorie_inventory_item:
-        highest_calories_held_by_a_single_elf = max(current_elf_calories, highest_calories_held_by_a_single_elf)
+        heapq.heappush(elf_calorie_totals_max_heap, -current_elf_calories)
         current_elf_calories = 0
         continue
 
     current_elf_calories += int(elf_calorie_inventory_item)
 
-print(highest_calories_held_by_a_single_elf)
+# Get sum of calories for top 3 elves
+sum_top_three_elf_calories = sum([
+    -heapq.heappop(elf_calorie_totals_max_heap),
+    -heapq.heappop(elf_calorie_totals_max_heap),
+    -heapq.heappop(elf_calorie_totals_max_heap),
+])
+
+print(sum_top_three_elf_calories)
