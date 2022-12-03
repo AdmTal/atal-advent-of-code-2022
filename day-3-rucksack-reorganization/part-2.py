368b667f3d3792773f4e08ac503a298419f4f24a
Adam Tal <admtal@gmail.com>
2022-12-03 17:14:57 -0500

    tweaks

--- day-3-rucksack-reorganization/part-2.py
+++ day-3-rucksack-reorganization/part-2.py
@@ -1,11 +1,9 @@
 rucksack_inventories = open('./input.txt').read().split('\n')
 
-GROUP_SIZE = 3
 
-
-def groups(array, chunk_size=GROUP_SIZE):
-    for i in range(0, len(array), chunk_size):
-        yield [set(x) for x in array[i:i + chunk_size]]
+def groups(array, group_size=3):
+    for i in range(0, len(array), group_size):
+        yield [set(x) for x in array[i:i + group_size]]
 
 
 def priority(letter):
