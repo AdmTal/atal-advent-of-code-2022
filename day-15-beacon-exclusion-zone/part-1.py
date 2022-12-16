741c4d216a440c8fc491b5da380dce4030a1c948
adam.tal@vimeo.com <adam.tal@vimeo.com>
2022-12-15 19:35:19 -0500

    Day 15 - brute force ...

--- day-15-beacon-exclusion-zone/part-1.py
+++ day-15-beacon-exclusion-zone/part-1.py
@@ -1,54 +1,57 @@
-input_items = open('./example-input.txt').read().split('\n')
-# input_items = open('./input.txt').read().split('\n')
+import re
+from collections import defaultdict
 
-print(input_items)
+# input_items = open('./example-input.txt').read().split('\n')
+input_items = open('./input.txt').read().split('\n')
 
-
-"""
-
-Come back and do this later ... I don't have the mind space for it now.
-
-It looks like ... if you know a Sensor's closest Beacon - you can assume the "square" area around it is empty of beacons.
-
-That might be - any COORD that has MANHATTAN_DISTANCE < the distance of the Beacon.
-
-RIGHT???
-
-S0... the answer might be:
-- Parse - set up grid
-- For each Sensor - find the closest Beacon - and Mark all of the # (area that cannot be a beacon)
-    - The trick here is likely a recursive func to find the "manhattan area" - all of the paths starting from S
-- Then, just count the #'s in a given row, that is the answer
-
-               1    1    2    2
-     0    5    0    5    0    5
--2 ..........#.................
--1 .........###................
- 0 ....S...#####...............
- 1 .......#######........S.....
- 2 ......#########S............
- 3 .....###########SB..........
- 4 ....#############...........
- 5 ...###############..........
- 6 ..#################.........
- 7 .#########S#######S#........
- 8 ..#################.........
- 9 ...###############..........
-10 ....B############...........
-11 ..S..###########............
-12 ......#########.............
-13 .......#######..............
-14 ........#####.S.......S.....
-15 B........###................
-16 ..........#SB...............
-17 ................S..........B
-18 ....S.......................
-19 ............................
-20 ............S......S........
-21 ............................
-22 .......................B....
+input_re = re.compile('Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)')
+sensor_beacon_pairs = []
+sensors = defaultdict(lambda: defaultdict(bool))
+beacons = defaultdict(lambda: defaultdict(bool))
+coords_that_are_totally_not_beacons_dude = defaultdict(lambda: defaultdict(bool))
+for line in input_items:
+    sx, sy, bx, by = input_re.match(line).groups()
+    sensor_beacon_pairs.append(((int(sx), int(sy)), (int(bx), int(by))))
+    sensors[sx][sy] = True
+    beacons[bx][by] = True
 
 
+def manhattan_distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
-"""
+POSSIBLE_DIRECTIONS = [(1, 0), (-1, 0)]
+
+TARGET_Y = 2_000_000
+
+
+def generate_all_coords_within_manhattan_distance(sx, sy, max_manhattan_distance):
+    coords = []
+    to_visit = [(sx, sy)]
+    visited = defaultdict(lambda: defaultdict(bool))
+    while to_visit:
+        x, y = to_visit.pop(0)
+        coords.append((x, TARGET_Y))
+        visited[x][y] = True
+        for dx, dy in POSSIBLE_DIRECTIONS:
+            next_x, next_y = x + dx, y + dy
+            next_y = TARGET_Y
+            next_manhattan_dist = manhattan_distance(sx, sy, next_x, next_y)
+            next_is_beacon = beacons[next_x][next_y]
+            if not visited[next_x][next_y] and not next_is_beacon and next_manhattan_dist <= max_manhattan_distance:
+                to_visit.append((next_x, next_y))
+
+    return coords
+
+
+# We can observe each sensor to get a list of spaces that CANNOT be beacons
+for s_coord, b_coord in sensor_beacon_pairs:
+    sx, sy = s_coord
+    bx, by = b_coord
+    manhattan_dist = manhattan_distance(sx, sy, bx, by)
+    coords = generate_all_coords_within_manhattan_distance(sx, sy, manhattan_dist)
+    for x, y in coords:
+        coords_that_are_totally_not_beacons_dude[y][x] = True
+
+positions_that_cannot_be_a_beacon = sum(coords_that_are_totally_not_beacons_dude[TARGET_Y].values()) - 1
+print(positions_that_cannot_be_a_beacon)
---
+++ day-15-beacon-exclusion-zone/part-2.py
@@ -0,0 +1,67 @@
+import re
+from collections import defaultdict
+
+# input_items = open('./example-input.txt').read().split('\n')
+input_items = open('./input.txt').read().split('\n')
+
+input_re = re.compile('Sensor at x=([-0-9]+), y=([-0-9]+): closest beacon is at x=([-0-9]+), y=([-0-9]+)')
+sensor_beacon_pairs = []
+sensors = defaultdict(lambda: defaultdict(bool))
+beacons = defaultdict(lambda: defaultdict(bool))
+coords_that_are_totally_not_beacons_dude = defaultdict(lambda: defaultdict(bool))
+for line in input_items:
+    sx, sy, bx, by = input_re.match(line).groups()
+    sensor_beacon_pairs.append(((int(sx), int(sy)), (int(bx), int(by))))
+    sensors[sx][sy] = True
+    beacons[bx][by] = True
+
+
+def manhattan_distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+POSSIBLE_DIRECTIONS = [(1, 0), (-1, 0)]
+
+
+def generate_all_non_beacon_or_sensor_coords_within_manhattan_distance(sx, sy, max_manhattan_distance, target_y):
+    coords = []
+    to_visit = [(sx, sy)]
+    visited = defaultdict(lambda: defaultdict(bool))
+    while to_visit:
+        x, y = to_visit.pop(0)
+        coords.append((x, y))
+        visited[x][y] = True
+        for next_x in range(1, SEARCH_MAX + 1):
+            next_y = target_y
+            next_manhattan_dist = manhattan_distance(sx, sy, next_x, next_y)
+            next_is_beacon = beacons[next_x][next_y]
+            if not visited[next_x][next_y] and not next_is_beacon and next_manhattan_dist <= max_manhattan_distance:
+                to_visit.append((next_x, next_y))
+
+    return coords
+
+
+SEARCH_MAX = 4_000_000
+
+print('starting 1')
+
+# We can observe each sensor to get a list of spaces that CANNOT be beacons
+for idx, xxx in enumerate(sensor_beacon_pairs, start=1):
+    s_coord, b_coord = xxx
+    sx, sy = s_coord
+    bx, by = b_coord
+    print(f'{idx} / {len(sensor_beacon_pairs)}')
+    manhattan_dist = manhattan_distance(sx, sy, bx, by)
+    for i in range(1, SEARCH_MAX + 1):
+        coords = generate_all_non_beacon_or_sensor_coords_within_manhattan_distance(sx, sy, manhattan_dist, i)
+        for x, y in coords:
+            coords_that_are_totally_not_beacons_dude[y][x] = True
+
+print('starting 2')
+
+for x in range(1, SEARCH_MAX + 1):
+    for y in range(1, SEARCH_MAX + 1):
+        if not coords_that_are_totally_not_beacons_dude[y][x]:
+            answer = (x * 4_000_000) + y
+            print(answer)
+            exit()
